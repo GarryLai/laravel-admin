40462597057835cd676e5faad7bc464ad657af12
Garry Lai <garrylai0306@gmail.com>
2022-08-12 21:29:12 +0800

    Update CanExportGrid.php

--- src/Grid/Concerns/CanExportGrid.php
+++ src/Grid/Concerns/CanExportGrid.php
@@ -32,12 +32,6 @@ trait CanExportGrid
         }
 
         $this->disablePagination();
-            
-        if ($this->builder) {
-            call_user_func($this->builder, $this);
-        
-            return $this->getExporter($scope)->export();
-        }
         
         if ($forceExport) {
             $this->getExporter($scope)->export();
