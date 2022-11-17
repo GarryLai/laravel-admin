88e072bf12b42a759abd38fcd53c2cb5c49abaef
Garry Lai <garrylai0306@gmail.com>
2022-11-17 23:28:35 +0800

    Update Pjax.php

--- src/Middleware/Pjax.php
+++ src/Middleware/Pjax.php
@@ -53,7 +53,8 @@ class Pjax
 
         (new static())->handle(Request::capture(), $next)->send();
 	
-        abort(response(''));
+        //abort(response(''));
+        abort($response);
     }
 
     /**
